a73e2f4725706da21eff948a8dd2670eb89b9156
kwkwok1980 <kwkwok1980@gmail.com>
2017-02-23 12:42:40 +0800

    Update a_very_big_sum.cpp

--- a_very_big_sum.cpp
+++ a_very_big_sum.cpp
@@ -1,1 +1,24 @@
-#include <cmath>#include <cstdio>#include <vector>#include <iostream>#include <algorithm>using namespace std;int main() {    int n;    std::string s;        std::getline(std::cin, s);    n = ::atoi(s.c_str());        int i;    long long int sum = 0;    while(std::getline(std::cin, s, ' '))    {        i = ::atoi(s.c_str());        //std::cout << "-" << i << "-" << std::endl;        sum += i;    }        std::cout << sum << std::endl;        return 0;}
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() 
+{
+    int n;    
+    std::string s;        
+    std::getline(std::cin, s);    
+    n = ::atoi(s.c_str());        
+    int i;    
+    long long int sum = 0;    
+    while(std::getline(std::cin, s, ' '))    
+    {        
+        i = ::atoi(s.c_str());        
+        //std::cout << "-" << i << "-" << std::endl;        
+        sum += i;    
+    }        
+    std::cout << sum << std::endl;        
+    return 0;
+}

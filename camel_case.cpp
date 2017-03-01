24d1238f03f870c655ff1f0482fc7f2c763d8e35
kwkwok1980 <kwkwok1980@gmail.com>
2017-03-01 12:54:27 +0800

    Update camel_case.cpp

--- camel_case.cpp
+++ camel_case.cpp
@@ -1,1 +1,21 @@
-#include <cmath>#include <cstdio>#include <vector>#include <iostream>#include <algorithm>using namespace std;int main() {    std::string s;         std::getline(std::cin, s);    int count = 1;    for(auto& c : s)    {        if (c >='A' && c <='Z')        {         ++count;        }        //std::cout << c << std::endl;        }        std::cout << count << std::endl;    return 0;}
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+  std::string s;
+  std::getline(std::cin, s);
+  int count = 1;
+  for(auto& c : s)    
+  {        
+    if (c >='A' && c <='Z')
+    {
+      ++count;
+    }
+    //std::cout << c << std::endl;
+  }
+  std::cout << count << std::endl;
+  return 0;
+}

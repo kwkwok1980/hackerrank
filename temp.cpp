82ad23bc5f3c7c9f2087b66f9dedfcb65a4d4eb3
kwkwok1980 <kwkwok1980@gmail.com>
2017-03-24 08:30:29 +0800

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -1,137 +1,73 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
+#include <vector>
 #include <algorithm>
-using namespace std;
+#include <tuple>
 
-struct Count
+auto SuffixArray(const std::string& s)
 {
-    int A;
-    int C;
-    int G;
-    int T;
+    int n = s.length();
+    std::vector<int> sa(n);
+    std::vector<int> rank(n);
     
-    void Print()
+    for(int i=0; i<n; ++i)
     {
-        std::cout << A << "," << C << "," << G << "," << T << std::endl;
+        sa[i] = i;
+        rank[i] = s[i];
     }
     
-    void Set(char c, int i)
+    for(int k=1; k<n; k=k*2)
     {
-        switch(c)
+        auto func = [&](int i, int j)
         {
-            case 'A': A=i; break;
-            case 'C': C=i; break;
-            case 'G': G=i; break;
-            default: T=i; break;
-        }
-    }
-    
-    int Get(char c)
-    {
-        switch(c)
-        {
-            case 'A': return A;
-            case 'C': return C;
-            case 'G': return G;
-            default: return T;
-        }
-    }
-    
-    void Add(char c, int i)
-    {
-        switch(c)
-        {
-            case 'A': A=A+i; break;
-            case 'C': C=C+i; break;
-            case 'G': G=G+i; break;
-            default: T=T+i; break;
-        }
-    }
-     
-    bool operator==(const Count& count)
-    {
-        return A==count.A && C==count.C && G==count.G && T==count.T;
-    }
+            auto pair1 = std::make_pair(rank[i], (i+k) < n ? rank[i+k] : -1);
+            auto pair2 = std::make_pair(rank[j], (j+k) < n ? rank[j+k] : -1);
+            return pair1 < pair2;
+        };
+        std::sort(sa.begin(), sa.end(), func);
         
-};
-
-int main() 
-{
-    std::string s;
-    char genes[4] = {'A','C','G','T'}; 
-    
-    std::getline(std::cin, s);
-    int size = atoi(s.c_str());
-    
-    std::getline(std::cin, s);
-    int div = size/4;
-    
-    Count target{-div,-div,-div,-div};
-    for (int i=0; i<size; ++i)
-    {
-        target.Add(s[i],1);
-    }
-    
-    for(char gene : genes)
-    {
-        if (target.Get(gene) < 0)
+        std::vector<int> dp(n);
+        for (int i=0; i<n; ++i)
         {
-            target.Set(gene, 0);    
-        }
-    }
-    
-    //target.Print();
-    int min = size;
-    
-    Count count {0,0,0,0};
-    int begin = 0;
-    int end = 4;
-    
-    for(int i=0; i<4; ++i)
-    {
-        char c = s[i];
-        if (target.Get(c) != 0)
-        {
-            count.Add(c,1);    
-        }
-    }
-    
-    while(end < size)
-    {
-        if (count == target)
-        {
-                
-        }
-    }
-    
-    
-    for (int i=0; i<size; ++i)
-    {
-        Count count {0,0,0,0};
-        for (int j=i; j<size; ++j)
-        {
-            char c = s[j];
-            if (target.Get(c) > 0)
+            if (i==0)
             {
-                count.Add(c, 1);
-                if (count.Get(c) > target.Get(c))
-                {
-                    break;    
-                }
-                
-                if (count == target)
-                {
-                    int len = j-i+1;
-                    min = std::min(min, len);
-                    break;
-                }
+                dp[sa[i]] = 0; 
+            }
+            else 
+            {
+                dp[sa[i]] = dp[sa[i-1]] + func(i-1,i);
             }
         }
+        rank = std::move(dp);
     }
+    return std::make_pair(sa, rank);
+}
+
+
+void Solve()
+{
+    std::string s;
+    int n, a, b;
+    std::cin >> n >> a >> b;
+    std::cin >> s;
+    std::cout << n << " " << a << " " << b << " " << s << std::endl;
     
-    std::cout << min << std::endl;
+    std::vector<int> sa, rank;
+    std::tie(sa, rank) = SuffixArray(s);
+    for(auto& i : sa) std::cout << i << std::endl;
     
+    
+    
+    
+}
+
+int main() {
+    int t; 
+    std::cin >> t;
+    
+    for(int i=0; i<t; ++i)
+    {
+        Solve();    
+    
+    }
     return 0;
 }

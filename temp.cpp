7f12901da45702dfa18b3d890b84eef83791a330
kwkwok1980 <kwkwok1980@gmail.com>
2017-03-31 19:15:14 +0800

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -17,6 +17,14 @@ auto SuffixArray(const std::string& s)
     
     for(int k=1; k<n; k=k*2)
     {
+        /*
+        std::cout << "--- rank ----" << std::endl;
+        for (int i=0; i<n; ++i)
+        {
+            std::cout << i << ":" << rank[i] << std::endl;
+        }
+        */
+        
         auto func = [&](int i, int j)
         {
             auto pair1 = std::make_pair(rank[i], (i+k) < n ? rank[i+k] : -1);
@@ -25,21 +33,125 @@ auto SuffixArray(const std::string& s)
         };
         std::sort(sa.begin(), sa.end(), func);
         
+        /*
+        std::cout << "--- sa ----" << std::endl;
+        for(auto& i : sa) 
+        {
+            std::cout << i << ":";
+            for(int j=i; j<n; ++j)
+            {
+                std::cout << s[j];
+            }
+            std::cout << std::endl;
+        }
+        */
+        
         std::vector<int> dp(n);
         for (int i=0; i<n; ++i)
         {
             if (i==0)
             {
                 dp[sa[i]] = 0; 
+                //std::cout << "--- r0 ----" << sa[i] << ":" << dp[sa[i]] << std::endl;
             }
             else 
             {
-                dp[sa[i]] = dp[sa[i-1]] + func(i-1,i);
+                if (func(sa[i-1], sa[i]))
+                {
+                    dp[sa[i]] = dp[sa[i-1]] + 1;
+                    //std::cout << "--- r1 ----" << sa[i] << ":" << dp[sa[i]] << std::endl;
+                }
+                else
+                {
+                    dp[sa[i]] = dp[sa[i-1]];
+                    //std::cout << "--- r2 ----" << sa[i] << ":" << dp[sa[i]] << std::endl;
+                }
             }
+            
         }
         rank = std::move(dp);
     }
     return std::make_pair(sa, rank);
+}
+
+auto LongestCommonSuffix(const std::string& s)
+{
+    int n = s.length();
+    std::vector<int> sa, rank, len(n);
+    std::tie(sa, rank) = SuffixArray(s);
+    
+    
+    for(int i=0; i<n; ++i)
+    {
+        int maxlen = 0;
+        for(int j=rank[i]-1; j>=0; --j)
+        {
+            if (sa[j] > i)
+            {
+               continue;    
+            }
+            else
+            {
+                int l = 0;
+                for(int k=0; k<n; ++k)
+                {
+                    if (i+k<n && sa[j]+k<n)
+                    {
+                        if (s[i+k] == s[sa[j]+k])
+                        {
+                            ++l;    
+                        }
+                        else
+                        {
+                            break;    
+                        }
+                    }
+                    else
+                    {
+                        break;    
+                    }
+                }
+                maxlen = std::max(maxlen, l);
+                break;
+            }
+        }
+        
+        for(int j=rank[i]+1; j<n; ++j)
+        {
+            if (sa[j] > i)
+            {
+               continue;    
+            }
+            else
+            {
+                int l = 0;
+                for(int k=0; k<n; ++k)
+                {
+                    if (i+k<n && sa[j]+k<n)
+                    {
+                        if (s[i+k] == s[sa[j]+k])
+                        {
+                            ++l;
+                        }
+                        else
+                        {
+                            break;    
+                        }
+                    }
+                    else
+                    {
+                        break;    
+                    }
+                }
+                maxlen = std::max(maxlen, l);
+                break;
+            }
+        }
+        
+        len[i] = maxlen;
+    }
+    
+    return len;
 }
 
 
@@ -51,12 +163,30 @@ void Solve()
     std::cin >> s;
     std::cout << n << " " << a << " " << b << " " << s << std::endl;
     
-    std::vector<int> sa, rank;
+    std::vector<int> sa, rank, len;
     std::tie(sa, rank) = SuffixArray(s);
-    for(auto& i : sa) std::cout << i << std::endl;
     
+    /*
+    for(auto& i : sa) 
+    {
+        std::cout << i << ":";
+        for(int j=i; j<n; ++j)
+        {
+            std::cout << s[j];
+        }
+        std::cout << std::endl;
+    }
+    */
     
+    len = LongestCommonSuffix(s);
     
+    /*
+    std::cout << "---------------" << std::endl;
+    for(int i=0; i<n; ++i)
+    {
+        std::cout << i << ":" << len[i] << std::endl;
+    }
+    */
     
 }
 
@@ -67,7 +197,6 @@ int main() {
     for(int i=0; i<t; ++i)
     {
         Solve();    
-    
     }
     return 0;
 }

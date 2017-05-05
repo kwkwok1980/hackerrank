704ba0350460baa023f5b47260f628d35b3b35b1
kwkwok1980 <kwkwok1980@gmail.com>
2017-05-05 15:47:45 +0800

    Create caesar_cipher.java

---
+++ caesar_cipher.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String s = scanner.next();
+        int k = scanner.nextInt();
+        
+        StringBuffer sb = new StringBuffer();
+        for(int i=0; i<n; ++i)
+        {
+            int c = s.charAt(i);
+            if (c >= 'A' && c <= 'Z')
+            {
+                c = 'A' + (c-'A'+k)%26;
+                sb.append((char)c);
+            }
+            else if (c >= 'a' && c <= 'z')
+            {
+                c = 'a' + (c-'a'+k)%26;
+                sb.append((char)c);
+            }
+            else
+            {
+                sb.append((char)c);    
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}

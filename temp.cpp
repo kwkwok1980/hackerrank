84c0e2fcda95b634eca6460ff3717a451cc335f9
kwkwok1980 <kwkwok1980@gmail.com>
2017-08-23 10:21:48 +0800

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -1,231 +1,100 @@
-#include <iostream>
+#include <cmath>
+#include <cstdio>
 #include <vector>
+#include <iostream>
 #include <algorithm>
-#include <tuple>
-#include <limits> 
+using namespace std;
 
-auto SuffixArray(const std::string& s)
+int Points [25][25];
+
+struct Point
 {
-    int n = s.length();
-    std::vector<int> sa(n);
-    std::vector<int> rank(n);
+    int x;
+    int y;
+};
+
+struct Step
+{
+    int a;
+    int b;
+};
+
+void Walk(int n, const Step& step, const Point& point)
+{
+    int i,j;
+    int d = Points[point.x][point.y] + 1;
     
-    for(int i=0; i<n; ++i)
-    {
-        sa[i] = i;
-        rank[i] = s[i];
-    }
+    i = point.x + step.a;
+    j = point.y + step.b;
+    if (i<n && j<n && i>=0 && j>=0 && (Points[i][j] == -1 || Points[i][j] > d)) Points[i][j] = d;
     
-    for(int k=1; k<n; k=k*2)
-    {
-        /*
-        std::cout << "--- rank ----" << std::endl;
-        for (int i=0; i<n; ++i)
-        {
-            std::cout << i << ":" << rank[i] << std::endl;
-        }
-        */
+    i = point.x + step.a;
+    j = point.y - step.b;
+    if (i<n && j<n && i>=0 && j>=0 && (Points[i][j] == -1 || Points[i][j] > d)) Points[i][j] = d;
+    
+    i = point.x - step.a;
+    j = point.y + step.b;
+    if (i<n && j<n && i>=0 && j>=0 && (Points[i][j] == -1 || Points[i][j] > d)) Points[i][j] = d;
+    
+    i = point.x - step.a;
+    j = point.y - step.b;
+    if (i<n && j<n && i>=0 && j>=0 && (Points[i][j] == -1 || Points[i][j] > d)) Points[i][j] = d;
         
-        auto func = [&](int i, int j)
-        {
-            auto pair1 = std::make_pair(rank[i], (i+k) < n ? rank[i+k] : -1);
-            auto pair2 = std::make_pair(rank[j], (j+k) < n ? rank[j+k] : -1);
-            return pair1 < pair2;
-        };
-        std::sort(sa.begin(), sa.end(), func);
-        
-        /*
-        std::cout << "--- sa ----" << std::endl;
-        for(auto& i : sa) 
-        {
-            std::cout << i << ":";
-            for(int j=i; j<n; ++j)
-            {
-                std::cout << s[j];
-            }
-            std::cout << std::endl;
-        }
-        */
-        
-        std::vector<int> dp(n);
-        for (int i=0; i<n; ++i)
-        {
-            if (i==0)
-            {
-                dp[sa[i]] = 0; 
-                //std::cout << "--- r0 ----" << sa[i] << ":" << dp[sa[i]] << std::endl;
-            }
-            else 
-            {
-                if (func(sa[i-1], sa[i]))
-                {
-                    dp[sa[i]] = dp[sa[i-1]] + 1;
-                    //std::cout << "--- r1 ----" << sa[i] << ":" << dp[sa[i]] << std::endl;
-                }
-                else
-                {
-                    dp[sa[i]] = dp[sa[i-1]];
-                    //std::cout << "--- r2 ----" << sa[i] << ":" << dp[sa[i]] << std::endl;
-                }
-            }
-            
-        }
-        rank = std::move(dp);
-    }
-    return std::make_pair(sa, rank);
+    i = point.x + step.b;
+    j = point.y + step.a;
+    if (i<n && j<n && i>=0 && j>=0 && (Points[i][j] == -1 || Points[i][j] > d)) Points[i][j] = d;
+    
+    i = point.x + step.b;
+    j = point.y - step.a;
+    if (i<n && j<n && i>=0 && j>=0 && (Points[i][j] == -1 || Points[i][j] > d)) Points[i][j] = d;
+    
+    i = point.x - step.b;
+    j = point.y + step.a;
+    if (i<n && j<n && i>=0 && j>=0 && (Points[i][j] == -1 || Points[i][j] > d)) Points[i][j] = d;
+    
+    i = point.x - step.b;
+    j = point.y - step.a;
+    if (i<n && j<n && i>=0 && j>=0 && (Points[i][j] == -1 || Points[i][j] > d)) Points[i][j] = d;
 }
 
-auto LongestCommonSuffix(const std::string& s)
+int Fill(int n, const Step& step)
 {
-    int n = s.length();
-    std::vector<int> sa, rank, len(n);
-    std::tie(sa, rank) = SuffixArray(s);
-    
-    
-    for(int i=0; i<n; ++i)
-    {
-        int maxlen = 0;
-        for(int j=rank[i]-1; j>=0; --j)
-        {
-            if (sa[j] > i)
-            {
-               continue;    
-            }
-            else
-            {
-                int l = 0;
-                for(int k=0; k<n; ++k)
-                {
-                    if (i+k<n && sa[j]+k<i)
-                    {
-                        if (s[i+k] == s[sa[j]+k])
-                        {
-                            ++l;    
-                        }
-                        else
-                        {
-                            break;    
-                        }
-                    }
-                    else
-                    {
-                        break;    
-                    }
-                }
-                maxlen = std::max(maxlen, l);
-                break;
-            }
-        }
-        
-        for(int j=rank[i]+1; j<n; ++j)
-        {
-            if (sa[j] > i)
-            {
-               continue;    
-            }
-            else
-            {
-                int l = 0;
-                for(int k=0; k<n; ++k)
-                {
-                    if (i+k<n && sa[j]+k<i)
-                    {
-                        if (s[i+k] == s[sa[j]+k])
-                        {
-                            ++l;
-                        }
-                        else
-                        {
-                            break;    
-                        }
-                    }
-                    else
-                    {
-                        break;    
-                    }
-                }
-                maxlen = std::max(maxlen, l);
-                break;
-            }
-        }
-        
-        len[i] = maxlen;
-    }
-    
-    return len;
-}
-
-
-void Solve()
-{
-    std::string s;
-    int n, a, b;
-    std::cin >> n >> a >> b;
-    std::cin >> s;
-    //std::cout << n << " " << a << " " << b << " " << s << std::endl;
-    
-    std::vector<int> sa, rank, len;
-    std::tie(sa, rank) = SuffixArray(s);
-    
-    /*
-    for(auto& i : sa) 
-    {
-        std::cout << i << ":";
-        for(int j=i; j<n; ++j)
-        {
-            std::cout << s[j];
-        }
-        std::cout << std::endl;
-    }
-    */
-    
-    len = LongestCommonSuffix(s);
-    
-    /*
-    std::cout << "---------------" << std::endl;
-    for(int i=0; i<n; ++i)
-    {
-        std::cout << i << ":" << len[i] << std::endl;
-    }
-    std::cout << "---------------" << std::endl;
-    */
-    
-    std::vector<int> results(n);
     for (int i=0; i<n; ++i)
     {
-        results[i] = std::numeric_limits<int>::max();    
-    }
-    
-    results[0] = a;
-    for (int i=1; i<n; ++i)
-    {
-        results[i] = std::min(results[i], results[i-1]+a);
-        for(int j=0; j<len[i]; ++j)
+        for (int j=0; j<n; ++j)
         {
-            results[i+j] = std::min(results[i+j], results[i-1]+b);
+            Points[i][j] = -1;
         }
     }
     
-    /*
-    for(int i=0; i<n; ++i)
+    Points[n-1][n-1] = 0;
+    for (int i=n-1; i>=0; --i)
     {
-        std::cout << results[i] << std::endl;
+        for (int j=n-1; j>=0; --j)
+        {
+            if (Points[i][j] != -1)
+            {
+                Point point {i, j};
+                Walk(n, step, point);
+            }
+        }
     }
-    */
-    
-    std::cout << results[n-1] << std::endl;
-    
-    
-    
+    return Points[0][0];
 }
-
-int main() {
-    int t; 
-    std::cin >> t;
     
-    for(int i=0; i<t; ++i)
+
+int main() 
+{
+    int n;
+    std::cin >> n;
+    for (int i=1; i<n; ++i)
     {
-        Solve();    
+        for (int j=i; j<n; ++j)
+        {
+            Step step {i, j};
+            int d = Fill(n, step);
+            std::cout << i << "," << j << "," << d << std::endl;
+        }
     }
     return 0;
 }

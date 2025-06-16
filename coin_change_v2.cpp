5285012e8572767c3acca9a23070ae242a059704
kwkwok1980 <kwkwok1980@gmail.com>
2025-06-16 21:38:51 +0800

    Update coin_change_v2.cpp

--- coin_change_v2.cpp
+++ coin_change_v2.cpp
@@ -4,17 +4,17 @@ long getWays(int N, std::vector<long> coins) {
     int M = coins.size();
     long results[250+1][50+1]{};
     for (int n=0; n<=N; ++n) {
-        for (int m=M-1; m>=0; --m) {
+        for (int m=0; m<M; ++m) {
             long coin = coins[m];
             long result = 0;
-            if (m == M-1) {
+            if (m == 0) {
                 if (coin == n) {
                     result = 1;
                 } else if (n > coin) {
                     result = result + results[n-coin][m];
                 }
             } else {
-                result = results[n][m+1];
+                result = results[n][m-1];
                 if (n == coin) {
                     result = result + 1;
                 } else if (n > coin) {
@@ -24,5 +24,5 @@ long getWays(int N, std::vector<long> coins) {
             results[n][m] = result;
         }
     }
-    return results[N][0];
+    return results[N][M-1];
 }
